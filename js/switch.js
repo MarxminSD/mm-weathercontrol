002634d9d8014327c5eaab99c52bdaae5827de4b
Marxmin <96161173+MarxminSD@users.noreply.github.com>
2023-11-17 18:37:09 -0500

    Added Slider

---
+++ js/switch.js
@@ -0,0 +1,10 @@
+document.getElementById('toggle-switch').addEventListener('change', function() {
+    if(this.checked) {
+      console.log('Toggle switch is ON');
+      // Perform action when checked
+    } else {
+      console.log('Toggle switch is OFF');
+      // Perform action when unchecked
+    }
+  });
+  
